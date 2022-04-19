2d424be64dc63a62718ad5838512eb7167627128
dan929873 <metallica04@yandex.ru>
2022-04-19 11:11:54 +0300

    hw4

--- MyToDO/user/views.py
+++ MyToDO/user/views.py
@@ -1,8 +1,18 @@
-from rest_framework.viewsets import ModelViewSet
+from django.shortcuts import get_object_or_404
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework import viewsets
 from user.models import User
+from rest_framework import mixins
 from user.serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin, GenericViewSet):
     serializer_class = UserModelSerializer
     queryset = User.objects.all()
+
+
+
+
+
