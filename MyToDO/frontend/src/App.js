6439a75eb20f7f85fc72761623d6a0c50b5251bb
dan929873 <metallica04@yandex.ru>
2022-04-25 13:53:33 +0300

    hw5

--- MyToDO/frontend/src/App.js
+++ MyToDO/frontend/src/App.js
@@ -3,18 +3,19 @@ import axios from 'axios';
 import UserList from './components/UserList.js';
 import ProjectList from './components/ProjectList.js';
 import TODOList from './components/TODOList.js';
-//import {HashRouter, BrowserRouter, Route, Routes, Link, Navigate, useLocation} from 'react-router-dom'
+import {HashRouter, BrowserRouter, Route, Routes, Link, Navigate, useLocation} from 'react-router-dom'
 
 
-//const NotFound = () => {
-//    var location = useLocation()
-//
-//    return (
-//        <div>
-//            Page "{location.pathname}" not found
-//        </div>
-//    )
-//}
+
+const NotFound = () => {
+    var location = useLocation()
+
+    return (
+        <div>
+            Page "{location.pathname}" not found
+        </div>
+    )
+}
 
 
 class App extends React.Component {
@@ -40,7 +41,7 @@ class App extends React.Component {
         axios
             .get('http://127.0.0.1:8000/api/project/')
             .then(response => {
-                const projects = response.data
+                const projects = response.data.results
                 this.setState({
                     'projects': projects
                 })
@@ -49,7 +50,7 @@ class App extends React.Component {
         axios
             .get('http://127.0.0.1:8000/api/todo/')
             .then(response => {
-                const todos = response.data
+                const todos = response.data.results
                 this.setState({
                     'todos': todos
                 })
@@ -61,10 +62,35 @@ class App extends React.Component {
 
     render(){
         return(
-            <div className="App">
-                <UserList users={this.state.users}/>
-                <ProjectList projects={this.state.projects}/>
-                <TODOList todos={this.state.todos}/>
+//            <div className="App">
+//                <HashRouter>
+//                    <Route exact path='/' component={() => <UserList users={this.state.users} />} />
+//
+//                    <Route exact path='/Project' component={() => <ProjectList projects={this.state.projects} />} />
+//
+//                    <Route exact path='/TODO' component={() => <TODOList todos={this.state.todos} />} />
+//                </HashRouter>
+//
+////                <UserList users={this.state.users}/>
+////                <ProjectList projects={this.state.projects}/>
+////                <TODOList todos={this.state.todos}/>
+//            </div>
+            <div>
+                <BrowserRouter>
+                    <nav>
+                        <li><Link to='/'>Users</Link></li>
+                        <li><Link to='/Project'>Projects</Link></li>
+                        <li><Link to='/TODO'>TODO</Link></li>
+                    </nav>
+
+                    <Routes>
+                        <Route exact path='/' element = {<UserList users={this.state.users} />} />
+                        <Route exact path='/Project' element = {<ProjectList projects={this.state.projects} />} />
+                        <Route exact path='/User' element = {<Navigate to='/' />} />
+                        <Route exact path='/TODO' element = {<TODOList todos={this.state.todos}/>}  />
+                        <Route path='*' element = {<NotFound />} />
+                    </Routes>
+                </BrowserRouter>
             </div>
         )
     }
