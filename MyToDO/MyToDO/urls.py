ea6332857df4c9afd8d4096b79b073b9a44c3d58
dan929873 <metallica04@yandex.ru>
2022-04-26 21:33:07 +0300

    hw6

--- MyToDO/MyToDO/urls.py
+++ MyToDO/MyToDO/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 from todo.views import ProjectModelViewSet, TODOModelViewSet
 from user.views import UserModelViewSet
@@ -30,4 +31,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
